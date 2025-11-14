c25f1b2e4990d1c894c22f3680f3f6a0600a07b6
kaltrina4 <kaltrine.heta@student.uni-pr.edu>
2025-11-14 14:49:29 +0100

    logjikë klienti për ngarkim dhe shkarkim skedarësh

--- Client.java
+++ Client.java
@@ -23,3 +23,41 @@ public class Client {
                 String response = in.readLine();
                 System.out.println(response);
                 boolean isAdmin = response.contains("ADMIN_GRANTED");
+
+                while (true) {
+                    System.out.print("Dergo mesazh/komande: ");
+                    String message = scanner.nextLine().trim();
+                    if (message.equalsIgnoreCase("exit")) break;
+
+                    if (!isAdmin && (message.startsWith("/upload") || message.startsWith("/delete"))) {
+                        System.out.println("️ Pa qasje (vetëm admin).");
+                        continue;
+                    }
+
+                    if (message.startsWith("/upload")) {
+                        String[] parts = message.split(" ", 2);
+                        if (parts.length < 2) {
+                            System.out.println("Përdorimi: /upload emri.txt");
+                            continue;
+                        }
+                        out.println(message);
+                        String signal = in.readLine();
+                        if ("OK_UPLOAD".equals(signal)) {
+                            System.out.println("Shkruaj përmbajtjen e file-it (shkruaj 'EOF' për fund):");
+                            while (true) {
+                                String line = scanner.nextLine();
+                                out.println(line);
+                                if ("EOF".equals(line)) break;
+                            }
+                        }
+                        System.out.println("Pergjigje nga server:\n" + readFullResponse(in));
+                    }
+
+                    else if (message.startsWith("/download")) {
+                        String[] parts = message.split(" ", 2);
+                        if (parts.length < 2) {
+                            System.out.println("Përdorimi: /download emri.txt");
+                            continue;
+                        }
+                        String fileName = parts[1].trim();
+                        out.println(message);
